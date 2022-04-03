548faf2fa215be738f0a4cb5b87a4c5af9c8f67c
Yulia2120 <ket-dz@rambler.ru>
2022-04-03 08:19:27 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MyNotepad__
+{
+
+        public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
